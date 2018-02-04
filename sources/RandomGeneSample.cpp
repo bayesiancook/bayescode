3721a784ad5e341db09d22701b4fd50fe6b06240
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2018-02-04 23:15:38 +0100

    random sub samples of genes

---
+++ sources/RandomGeneSample.cpp
@@ -0,0 +1,29 @@
+#include "Random.hpp"
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])    {
+
+    ifstream is(argv[1]);
+    int P = atoi(argv[2]);
+    ofstream os(argv[3]);
+    int N;
+    is >> N;
+    vector<string> gene(N);
+    for (int i=0; i<N; i++) {
+        is >> gene[i];
+    }
+
+    int* sample = new int[P];
+    Random::DrawFromUrn(sample,P,N);
+    os << P << '\n';
+    for (int i=0; i<P; i++) {
+        os << gene[sample[i]] << '\n';
+    }
+}
+
+
+
+
+
