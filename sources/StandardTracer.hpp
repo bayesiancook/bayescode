f9fda819d4f8cb61bc10b40b017129be94a1482a
Philippe Veber <philippe.veber@gmail.com>
2018-09-03 17:01:34 +0200

    StandardTracer: factor chain file name construction

--- sources/StandardTracer.hpp
+++ sources/StandardTracer.hpp
@@ -12,15 +12,18 @@ public:
                                                    stats_tracer(m,&M::declare_stats),
                                                    chain_name(chain_name) {}
     void start() {
-        std::ofstream model_os{chain_name + ".chain", std::ios_base::trunc};
+        std::ofstream model_os{chain_file(chain_name), std::ios_base::trunc};
         model_tracer.write_header(model_os);
         std::ofstream stats_os{chain_name + ".trace", std::ios_base::trunc};
         stats_tracer.write_header(stats_os);
     }
     void savepoint(int) {
-        std::ofstream model_os{chain_name + ".chain", std::ios_base::app};
+        std::ofstream model_os{chain_file(chain_name), std::ios_base::app};
         model_tracer.write_line(model_os);
         std::ofstream trace_os{chain_name + ".trace", std::ios_base::app};
         stats_tracer.write_line(trace_os);
+    }
+    static std::string chain_file(std::string chain_name) {
+        return chain_name + ".chain";
     }
 };
