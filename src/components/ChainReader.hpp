c4432cb03b13a8a1ed44add836458a6cc498e47d
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-09-18 14:56:34 +0200

    Added ChainReader from occigen-trace branch.

---
+++ src/components/ChainReader.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Tracer.hpp"
+
+class ChainReader {
+    Tracer tracer;
+    std::ifstream is;
+
+  public:
+    template <class M>
+    ChainReader(M& model, std::string filename) : tracer(model, &M::declare_model), is(filename) {
+        tracer.ignore_header(is);
+    }
+
+    void next() { tracer.read_line(is); }
+    void skip(int n) { for(int i = 0; i < n; i++) next(); }
+};
