39cbe42a612610b5de222dd14e549477aeb7d7f9
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-10-08 14:59:20 +0200

    Fixed missing std:: in inference args file.

--- src/InferenceAppArgParse.hpp
+++ src/InferenceAppArgParse.hpp
@@ -1,12 +1,13 @@
 #pragma once
+
 #include "components/BaseArgParse.hpp"
 
 class InferenceAppArgParse : public BaseArgParse {
   public:
     InferenceAppArgParse(ChainCmdLine &cmd) : BaseArgParse(cmd) {}
-    ValueArg<string> alignment{
+    ValueArg<std::string> alignment{
         "a", "alignment", "Alignment file (PHYLIP)", true, "", "string", cmd};
-    ValueArg<string> treefile{"t", "tree", "Tree file (NHX)", true, "", "string", cmd};
+    ValueArg<std::string> treefile{"t", "tree", "Tree file (NHX)", true, "", "string", cmd};
     ValueArg<int> every{
         "e", "every", "Number of iterations between two traces", false, 1, "int", cmd};
     ValueArg<int> until{"u", "until", "Maximum number of (saved) iterations (-1 means unlimited)",
