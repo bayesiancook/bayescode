833c14cba63d713be0713efc3c3d3568787a254a
Philippe Veber <philippe.veber@gmail.com>
2018-10-08 13:36:19 +0200

    missing pragma incantations

--- src/InferenceAppArgParse.hpp
+++ src/InferenceAppArgParse.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "components/BaseArgParse.hpp"
 
 class InferenceAppArgParse : public BaseArgParse {
--- src/components/StandardTracer.hpp
+++ src/components/StandardTracer.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <fstream>
 #include "ChainComponent.hpp"
 
@@ -12,13 +13,13 @@ class StandardTracer : public ChainComponent {
         : model_tracer(m, &M::declare_model),
           stats_tracer(m, &M::declare_stats),
           chain_name(chain_name) {}
-    void start() {
+    void start() final {
         std::ofstream model_os{chain_file(chain_name), std::ios_base::trunc};
         model_tracer.write_header(model_os);
         std::ofstream stats_os{chain_name + ".trace", std::ios_base::trunc};
         stats_tracer.write_header(stats_os);
     }
-    void savepoint(int) {
+    void savepoint(int) final {
         std::ofstream model_os{chain_file(chain_name), std::ios_base::app};
         model_tracer.write_line(model_os);
         std::ofstream trace_os{chain_name + ".trace", std::ios_base::app};
