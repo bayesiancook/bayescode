5121e15c8c79cbe1f7120c9803c31df7c35becf0
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2018-10-12 16:35:14 +0200

    imported codonm2a in new dev branch

---
+++ src/CodonM2a.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <fstream>
+#include "InferenceAppArgParse.hpp"
+#include "CodonM2aModel.hpp"
+#include "components/ChainCheckpoint.hpp"
+#include "components/ChainDriver.hpp"
+#include "components/ConsoleLogger.hpp"
+#include "components/StandardTracer.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    ChainCmdLine cmd{argc, argv, "CodonM2a", ' ', "0.1"};
+
+    ChainDriver *chain_driver = nullptr;
+    CodonM2aModel *model = nullptr;
+
+    if (cmd.resume_from_checkpoint()) {
+        std::ifstream is = cmd.checkpoint_file();
+        chain_driver = new ChainDriver(is);
+        model = new CodonM2aModel(is);
+    } else {
+
+        InferenceAppArgParse args(cmd);
+
+        TCLAP::ValueArg<double> pi{"p", "pi", "prior prob of being under positive selection (default: 0.1)",
+            false, 0.1, "double", cmd.get()};
+
+        cmd.parse();
+        chain_driver =
+            new ChainDriver(cmd.chain_name(), args.every.getValue(), args.until.getValue());
+        model = new CodonM2aModel(args.alignment.getValue(), args.treefile.getValue(), pi.getValue());
+    }
+
+    ConsoleLogger console_logger;
+    ChainCheckpoint chain_checkpoint(cmd.chain_name() + ".param", *chain_driver, *model);
+    StandardTracer trace(*model, cmd.chain_name());
+    chain_driver->add(*model);
+    chain_driver->add(console_logger);
+    chain_driver->add(chain_checkpoint);
+    chain_driver->add(trace);
+    chain_driver->go();
+
+    delete chain_driver;
+    delete model;
+}
