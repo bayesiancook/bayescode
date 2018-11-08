8a5f20771596f060547dc9dc387428ae6865ee02
boussau <boussau@gmail.com>
2018-11-08 15:08:09 +0100

    Tracer: changed collection overloads
    add now recursively calls itself on items of a vector/SimpleArray/SimpleBidimArray
    collection. Easier to write, but may incur a big penalty on memory usage
    and execution time (not so much compared to IOs).

--- src/components/Tracer.hpp
+++ src/components/Tracer.hpp
@@ -71,56 +71,23 @@ class Tracer {
         set_from_stream.emplace_back([&d](std::istream& is) { is >> d; });
     }
 
-    void add(std::string const& name, std::vector<double>& v) {
-        header_to_stream.emplace_back([&v, name](std::ostream& os) {
-            size_t n = v.size();
-            if (n > 0) {
-                os << name << "[0]";
-                for (size_t i = 1; i < n; i++) os << "\t" << name << "[" << i << "]";
-            }
-        });
-        data_to_stream.emplace_back([&v](std::ostream& os) {
-            size_t n = v.size();
-            if (n > 0) {
-                os << v.at(0);
-                for (size_t i = 1; i < n; i++) os << "\t" << v.at(i);
-            }
-        });
-        set_from_stream.emplace_back([&v](std::istream& is) {
-            for (auto& e : v) is >> e;
-        });
+    template <class T>
+    void add(std::string name, std::vector<T>& v) {
+      for(size_t i = 0; i < v.size(); i++)
+        add(name + "[" + std::to_string(i) + "]", v[i]);
     }
 
     template <class T>
-    void add(std::string const& name, SimpleArray<T>& v) {
-        header_to_stream.push_back([&v, name](std::ostream& os) {
-            int n = v.GetSize();
-            if (n > 0) {
-                os << name << "[0]";
-                for (int i = 1; i < n; i++) os << "\t" << name << "[" << i << "]";
-            }
-        });
-        data_to_stream.push_back([&v](std::ostream& os) { os << v; });
-        set_from_stream.push_back([&v](std::istream& is) { is >> v; });
+    void add(std::string name, SimpleArray<T>& v) {
+      for(int i = 0; i < v.GetSize(); i++)
+        add(name + "[" + std::to_string(i) + "]", v[i]);
     }
 
     template <class T>
     void add(std::string name, SimpleBidimArray<T>& mat) {
-      header_to_stream.push_back([&mat, name](std::ostream& os) {
-        int m = mat.GetNrow();
-        int n = mat.GetNcol();
-        if (m > 0 && n > 0) {
-          os << name << "[0][0]";
-          for (int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-              if(!(i == 0 && j == 0))
-              os << "\t" << name << "[" << i << "][" << j << "]";
-            }
-          }
-        }
-      });
-      data_to_stream.push_back([&mat](std::ostream& os) { os << mat; });
-      set_from_stream.push_back([&mat](std::istream& is) { is >> mat; });
+      for (int i = 0; i < mat.GetNrow(); i++)
+        for(int j = 0; j < mat.GetNcol(); j++)
+          add(name + "[" + std::to_string(i) + "][" + std::to_string(j) + "]", mat(i, j));
     }
 
     void add(std::string const& name, StickBreakingProcess& sbp) {
