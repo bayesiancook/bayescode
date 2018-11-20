c72dbd17f456d5eefedef199f8b894c11504cf55
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-11-20 17:52:00 +0100

    Working reduce component!

--- src/mpi_components/mpi_test.cpp
+++ src/mpi_components/mpi_test.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include "broadcast.hpp"
 #include "gather.hpp"
+#include "reduce.hpp"
 
 using MPI::p;
 using namespace std;
@@ -9,6 +10,7 @@ struct DummyModel {
     double a{-1}, b{-1}, c{-1};
     vector<double> v;
     Partition partition{{"d", "e", "f"}, static_cast<size_t>(p->size) - 1, 1};
+    double d{-1}, e{-1};
 
     template <class C>
     void declare_model(C& ref) {
@@ -16,12 +18,14 @@ struct DummyModel {
         ref.add("b", b);
         ref.add("c", c);
         ref.add("v", v, partition);
+        ref.add("d", d);
+        ref.add("e", e);
     }
 
     void print() {
         stringstream ss;
         for (auto e : v) { ss << e << " "; }
-        p->message("Model state is %f, %f, %f, {%s}", a, b, c, ss.str().c_str());
+        p->message("Model state is %f, %f, %f, {%s}, %f, %f", a, b, c, ss.str().c_str(), d, e);
     }
 };
 
@@ -33,13 +37,17 @@ void compute(int, char**) {
         m.c = 4.4;
     } else {  // slave
         m.v = std::vector<double>(m.partition.my_partition_size(), p->rank + 1.1);
+        m.d = p->rank + 0.4;
+        m.e = p->rank - 0.4;
     }
 
+    auto reducer = reduce_model(m, {"d", "e"});
     auto gatherer = gather_model(m, {"v"});
     auto bcaster = broadcast_model(m, {"a", "c"});
 
     p->rank ? bcaster->acquire() : bcaster->release();
     p->rank ? gatherer->release() : gatherer->acquire();
+    p->rank ? reducer->release() : reducer->acquire();
 
     m.print();
 }
