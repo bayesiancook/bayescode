8b92b66d3d5baf1ad44ec3d56b0d8fb50065305b
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-11-21 15:01:04 +0100

    Added macros to ease declaration of struct types for MPI

--- src/mpi_components/utils.hpp
+++ src/mpi_components/utils.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
 #include <mpi.h>
+#include <vector>
+
+#define STRUCT_DECL(CLASS)                          \
+    StructMetaData struct_decl_##CLASS;             \
+    StructMetaData& struct__ = struct_decl_##CLASS; \
+    struct__.member_types.push_back(MPI_UB);        \
+    struct__.offsets.push_back(sizeof(CLASS));      \
+    CLASS x__;
+#define ATTRIBUTE(name)                                                  \
+    struct__.member_types.push_back(get_datatype<decltype(x__.name)>()); \
+    struct__.offsets.push_back(offsetof(decltype(x__), name));
+
+
+struct StructMetaData {
+    std::vector<MPI_Datatype> member_types;
+    std::vector<MPI_Aint> offsets;
+};
 
 template <typename T>
 MPI_Datatype get_datatype() {
