6bc58cd3b73e6f227b93cc601acdb683f6884892
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-11-21 16:21:07 +0100

    Fixed a bug where structs with undeclared fields could be improperly transmitted over mpi (+ added to test)

--- src/mpi_components/utils.hpp
+++ src/mpi_components/utils.hpp
@@ -37,13 +37,16 @@ struct StructMetaData {
     std::vector<MPI_Datatype> types;
     std::vector<MPI_Aint> offsets;
     std::vector<int> block_lengths;
+    MPI_Aint size;
 
     MPI_Datatype declare_and_commit() {
         assert(types.size() == offsets.size());
         assert(types.size() == block_lengths.size());
-        MPI_Datatype result;
+        MPI_Datatype tmp;
         MPI_Type_create_struct(
-            types.size(), block_lengths.data(), offsets.data(), types.data(), &result);
+            types.size(), block_lengths.data(), offsets.data(), types.data(), &tmp);
+        MPI_Datatype result;
+        MPI_Type_create_resized(tmp, 0, size, &result);
         MPI_Type_commit(&result);
         return result;
     }
@@ -52,9 +55,16 @@ struct StructMetaData {
 #define STRUCT_DECL(CLASS)                          \
     StructMetaData struct_decl_##CLASS;             \
     StructMetaData& struct__ = struct_decl_##CLASS; \
+    struct__.size = sizeof(CLASS);                  \
     CLASS x__;
 #define ATTRIBUTE(NAME)                                           \
     struct__.types.push_back(get_datatype<decltype(x__.NAME)>()); \
     struct__.offsets.push_back(offsetof(decltype(x__), NAME));    \
     struct__.block_lengths.push_back(1);
-#define STRUCT_DECL_COMMIT(NAME) NAME = struct__.declare_and_commit();
+#define STRUCT_COMMIT(NAME) NAME = struct__.declare_and_commit();
+#define STRUCT_GLOBAL_DECL(CLASS, TYPENAME)            \
+    MPI_Datatype TYPENAME;                             \
+    template <>                                        \
+    struct GetMPIDatatype<CLASS> {                     \
+        MPI_Datatype operator()() { return TYPENAME; } \
+    };
