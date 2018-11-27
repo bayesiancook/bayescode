efe6cf7e041ca53b27a3f7e7c13ebc4d986197ef
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-11-27 17:25:35 +0100

    Added a test for Group

--- src/operations/test.cpp
+++ src/operations/test.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-int a = 1;
-int b = 1;
-
 class MyProxy : public Proxy {
     int& target;
 
@@ -17,6 +14,8 @@ class MyProxy : public Proxy {
 };
 
 TEST_CASE("ForAll test") {
+    int a = 1;
+    int b = 1;
     MyProxy p1(a);
     MyProxy p2(b);
     MyProxy p3(a);
@@ -42,4 +41,32 @@ TEST_CASE("ForAll test") {
     group.release();
     CHECK(a == 120);
     CHECK(b == 10);
+}
+
+TEST_CASE("Group test") {
+    int a = 1;
+    int b = 1;
+    std::unique_ptr<Proxy> p1(dynamic_cast<Proxy*>(new MyProxy(b)));
+    // clang-format off
+    Group group(
+        std::unique_ptr<Proxy>(dynamic_cast<Proxy*>(new MyProxy(a))),
+        std::move(p1),
+        std::unique_ptr<Proxy>(dynamic_cast<Proxy*>(new MyProxy(b)))
+    );
+    // clang-format on
+    CHECK(p1.get() == nullptr);
+
+    group.acquire();  // 2 b and 1 a
+    group.release();
+    CHECK(a == 4);
+    CHECK(b == 12);
+
+    p1.reset(dynamic_cast<Proxy*>(new MyProxy(a)));
+    group.add(std::move(p1));
+    group.add(std::unique_ptr<Proxy>(dynamic_cast<Proxy*>(new MyProxy(b))));
+
+    group.acquire();  // 3 b and 2 a
+    group.release();
+    CHECK(a == 24);
+    CHECK(b == 120);
 }
