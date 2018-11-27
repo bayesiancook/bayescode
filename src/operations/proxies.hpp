10c714d751c6cea74cbfb5bf7d989b42af36b47c
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-11-27 17:05:07 +0100

    Added a ForAll class

--- src/operations/proxies.hpp
+++ src/operations/proxies.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
+#include <vector>
 #include "interfaces.hpp"
+
+// does not own its contents
+class ForAll : public Proxy {
+    std::vector<Proxy*> pointers;
+
+  public:
+    ForAll() = default;
+
+    template <class... Pointers>
+    ForAll(Pointers&&... pointers) : pointers({pointers...}) {}
+
+    void add(Proxy* pointer) { pointers.push_back(pointer); }
+
+    void acquire() final {
+        for (auto pointer : pointers) { pointer->acquire(); }
+    }
+
+    void release() final {
+        for (auto pointer : pointers) { pointer->release(); }
+    }
+};
