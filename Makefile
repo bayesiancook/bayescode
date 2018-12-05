f9f2bbcab6eb7c724c23cc3223d7c6fca13c098a
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-12-05 18:10:34 +0100

    Added 24h auto stop (based on mean over 5 iterations)

--- Makefile
+++ Makefile
@@ -7,4 +7,4 @@ format:
 
 .PHONY: test-mgdiffsel
 test-mgdiffsel: all
-	cd data/small_multigene/ && mpirun -np 3 ../multigenediffseldsparse -t tree.nwk.annotated -d verysmall.list -ncond 2 -x 1 1 tmp
+	cd data/small_multigene/ && mpirun -np 3 ../multigenediffseldsparse -t tree.nwk.annotated -d verysmall.list -ncond 2 -x 1 10 tmp
--- sources/MultiGeneChain.cpp
+++ sources/MultiGeneChain.cpp
@@ -4,6 +4,7 @@
 #include "Chrono.hpp"
 #include "MultiGeneProbModel.hpp"
 #include "monitoring.hpp"
+#include <list>
 
 std::unique_ptr<MonitorManager> gm(new MonitorManager());
 
@@ -101,7 +102,30 @@ void MultiGeneChain::Run() {
         mvfile << header.str() << "\n";
     };
 
+    list<double> last_it_times;
+    double max_time = 24 * 60 * 60 * 1000; // 24 hours
+    // double max_time = 3 * 60 * 1000; // 3 minutes
+
+    auto mean_and_trim = [this, &last_it_times] (double it_time) -> double {
+        last_it_times.push_back(it_time);
+        double sum = 0;
+        int count = 0;
+        for (auto t : last_it_times) {
+            count ++;
+            sum += t;
+        }
+        if (last_it_times.size() > 5) {
+            last_it_times.pop_front();
+        }
+        return sum / count;
+    };
+
+    //=============================================================================================
+    //  MAIN LOOP
+    //=============================================================================================
     if (!myid) {
+        Chrono total_time_chrono;
+        total_time_chrono.Start();
         while ((GetRunningStatus() != 0) && ((until == -1) || (size <= until))) {
             MasterSendRunningStatus(1);
             Chrono chrono;
@@ -109,14 +133,27 @@ void MultiGeneChain::Run() {
             Move();
             chrono.Stop();
 
+            double it_time = chrono.GetTime();
+            double mean_it_time = mean_and_trim(it_time);
+            total_time_chrono.Stop();
+            double total_time = total_time_chrono.GetTime();
+            int remaining_its = floor((max_time - total_time) / mean_it_time);
+
             ofstream check_os((name + ".time").c_str());
-            check_os << chrono.GetTime() << '\n';
-            cerr << "* Iteration " << size - 1 << ": " << chrono.GetTime() / 1000 << "s\n";
+            check_os << it_time << '\n';
+            cerr << "* Iteration " << size - 1 << ": " << it_time / 1000
+                 << "s (mean it time: " << mean_it_time / 1000 << "s; predicting "
+                 << remaining_its - 2 << " more iterations)\n";
 
             if (size == first_iteration) {
                 write_header();
             }
             write_line();
+
+            if (total_time + 3 * mean_it_time > max_time) {
+                printf("Appraoching max time! Stopping computation.\n");
+                break;
+            }
         }
         MasterSendRunningStatus(0);
         ofstream run_os((name + ".run").c_str());
