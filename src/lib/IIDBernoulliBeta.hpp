1757744c5c5ae5bc7c3787132eed9082af73d5be
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2018-12-12 19:43:48 +0100

    added IIDBeta and IIDBernoulliBeta in lib

---
+++ src/lib/IIDBernoulliBeta.hpp
@@ -0,0 +1,187 @@
+
+#pragma once
+
+#include "Array.hpp"
+#include "CodonSuffStat.hpp"
+#include "PoissonSuffStat.hpp"
+#include "Random.hpp"
+
+/**
+ * \brief A sufficient statistic for a collection of random variables, from a
+ * mixture of a point mass at 0 and a Beta distribution (see IIDBernoulliBeta)
+ */
+
+class BernoulliBetaSuffStat : public SuffStat {
+  public:
+    BernoulliBetaSuffStat() {}
+    ~BernoulliBetaSuffStat() {}
+
+    //! set suff stats to 0
+    void Clear() {
+        sumlog0 = 0;
+        sumlog1 = 0;
+        n0 = 0;
+        n1 = 0;
+    }
+
+    //! add the contribution of one beta variate equal to 0 to this suffstat
+    void AddNullSuffStat(int c = 1) { n0 += c; }
+
+    //! add the contribution of one beta variate > 0 to this suffstat
+    void AddPosSuffStat(double log0, double log1, int c = 1) {
+        sumlog0 += log0;
+        sumlog1 += log1;
+        n1 += c;
+    }
+
+    //! (*this) += from
+    void Add(const BernoulliBetaSuffStat &from) {
+        sumlog0 += from.GetSumLog0();
+        sumlog1 += from.GetSumLog1();
+        n0 += from.GetN0();
+        n1 += from.GetN1();
+    }
+
+    //! (*this) += from, operator version
+    BernoulliBetaSuffStat &operator+=(const BernoulliBetaSuffStat &from) {
+        Add(from);
+        return *this;
+    }
+
+    //! return log prob of suff stats, as a function of parameters of the mixture
+    //! and the Beta distribution
+    double GetLogProb(double pi, double mean, double invconc) const {
+        double alpha = mean / invconc;
+        double beta = (1.0 - mean) / invconc;
+        double logbern = n0 * log(1 - pi) + n1 * log(pi);
+        double logbeta = n1 * (Random::logGamma(alpha + beta) - Random::logGamma(alpha) -
+                               Random::logGamma(beta)) +
+                         (alpha - 1) * sumlog0 + (beta - 1) * sumlog1;
+        return logbern + logbeta;
+    }
+
+    //! get number of contributing variables that are == 0
+    int GetN0() const { return n0; }
+    //! get number of contributing variables that are > 0
+    int GetN1() const { return n1; }
+    //! get total sum of log(1-x_i), for all x_i's that are > 0
+    double GetSumLog0() const { return sumlog0; }
+    //! get total sum of log(x_i), for all x_i's that are > 0
+    double GetSumLog1() const { return sumlog1; }
+
+    template <class T>
+    void serialization_interface(T &x) {
+        x.add(sumlog0, sumlog1, n0, n1);
+    }
+
+  private:
+    double sumlog0;
+    double sumlog1;
+    int n0;
+    int n1;
+};
+
+template <>
+struct has_custom_serialization<BernoulliBetaSuffStat> : std::true_type {};
+
+/**
+ * \brief An array of IID random variables, from a mixture of a point mass at 0
+ * (weight 1-pi) and a Beta distribution (weight pi)
+ */
+
+class IIDBernoulliBeta : public SimpleArray<double> {
+  public:
+    //! constructor, parameterized by size, weight parameter pi of the mixture,
+    //! and parameters of the Beta (alpha and beta)
+    IIDBernoulliBeta(int insize, const double& inpi, const double& inmean, const double& ininvconc)
+        : SimpleArray<double>(insize), pi(inpi), mean(inmean), invconc(ininvconc) {
+        Sample();
+    }
+
+    ~IIDBernoulliBeta() {}
+
+    //! return alpha parameter of the Beta distribution
+    double GetAlpha() const { return mean / invconc; }
+    //! return beta parameter of the Beta distribution
+    double GetBeta() const { return (1-mean)/invconc; }
+
+    //! sample all entries from prior distribution
+    void Sample() {
+        for (int i = 0; i < GetSize(); i++) {
+            if (Random::Uniform() < pi) {
+                (*this)[i] = Random::BetaSample(GetAlpha(), GetBeta());
+            } else {
+                (*this)[i] = 0;
+            }
+        }
+    }
+
+    //! get number of entries that are equal to 0
+    int GetNullSet() const {
+        int tot = 0;
+        for (int i = 0; i < GetSize(); i++) {
+            if (!GetVal(i)) {
+                tot++;
+            }
+        }
+        return tot;
+    }
+
+    //! return log probability summed over all entries
+    double GetLogProb() const {
+        double total = 0;
+        for (int i = 0; i < GetSize(); i++) {
+            total += GetLogProb(i);
+        }
+        return total;
+    }
+
+    //! return log probability for entry i
+    double GetLogProb(int i) const {
+        double ret = 0;
+        if (!GetVal(i)) {
+            ret += log(1 - pi);
+        } else {
+            ret += log(pi);
+            ret += Random::logBetaDensity(GetVal(i), GetAlpha(), GetBeta());
+        }
+        return ret;
+    }
+
+    //! add all entries to sufficient statistic
+    void AddSuffStat(BernoulliBetaSuffStat &suffstat) {
+        for (int i = 0; i < GetSize(); i++) {
+            if (!GetVal(i)) {
+                suffstat.AddNullSuffStat(1);
+            } else {
+                suffstat.AddPosSuffStat(log(GetVal(i)), log(1 - GetVal(i)), 1);
+            }
+        }
+    }
+
+    //! get mean of all of those entries that are not 0
+    double GetPosMean() const {
+        int tot = 0;
+        double m1 = 0;
+        for (int i = 0; i < GetSize(); i++) {
+            if (GetVal(i)) {
+                m1 += GetVal(i);
+                tot++;
+            }
+        }
+        if (!tot) {
+            return 0;
+        }
+        m1 /= tot;
+        return m1;
+    }
+
+  protected:
+    const double& pi;
+    const double& mean;
+    const double& invconc;
+};
+
+template <>
+struct has_custom_serialization<IIDBernoulliBeta> : std::true_type {};
+
---
+++ src/lib/IIDBeta.hpp
@@ -0,0 +1,138 @@
+
+#pragma once
+
+#include "Array.hpp"
+#include "CodonSuffStat.hpp"
+#include "PoissonSuffStat.hpp"
+#include "Random.hpp"
+
+/**
+ * \brief A sufficient statistic for a collection of beta variates, as a
+ * function of the parameters of the beta distribution
+ *
+ * Suppose you have x = (x_i)_i=1..N, iid Beta(a,b).
+ * Then, p(X | a,b) can be expressed as a function of compact sufficient
+ * statistics: sum log(x_i)'s, sum log(1-x_i)'s and N. BetaSuffStat implements
+ * this idea, by providing methods for collecting these suff stats and returning
+ * the log prob for a given value for the a and b parameters.
+ */
+
+class BetaSuffStat : public SuffStat {
+  public:
+    BetaSuffStat() {}
+    ~BetaSuffStat() {}
+
+    //! set suff stats to 0
+    void Clear() {
+        sumlog0 = 0;
+        sumlog1 = 0;
+        n = 0;
+    }
+
+    //! add the contribution of one beta variate (x) to this suffstat
+    void AddSuffStat(double log0, double log1, int c = 1) {
+        sumlog0 += log0;
+        sumlog1 += log1;
+        n += c;
+    }
+
+    //! (*this) += from
+    void Add(const BetaSuffStat &from) {
+        sumlog0 += from.GetSumLog0();
+        sumlog1 += from.GetSumLog1();
+        n += from.GetN();
+    }
+
+    //! (*this) += from, operator version
+    BetaSuffStat &operator+=(const BetaSuffStat &from) {
+        Add(from);
+        return *this;
+    }
+
+    //! return log prob of suff stats, as a function of mean and inverse
+    //! concentration
+    double GetLogProb(double mean, double invconc) const {
+        double alpha = mean / invconc;
+        double beta = (1 - mean) / invconc;
+        return GetLogProb(alpha, beta);
+    }
+
+    //! return N (number of beta variates included in this suff stat)
+    int GetN() const { return n; }
+    //! return sum log(x_i)'s
+    double GetSumLog0() const { return sumlog0; }
+    //! return sum log(1-x_i)'s
+    double GetSumLog1() const { return sumlog1; }
+
+    template <class T>
+    void serialization_interface(T &x) {
+        x.add(sumlog0, sumlog1, n);
+    }
+
+  private:
+    double sumlog0;
+    double sumlog1;
+    int n;
+};
+
+template <>
+struct has_custom_serialization<BetaSuffStat> : std::true_type {};
+
+/**
+ * \brief An array of IID beta random variables
+ *
+ * Note that the parameters of the beta distribution are given by copy (not by
+ * ref) to the array. Thus, each time these parameters are modified during the
+ * MCMC, the new values should be given to the array (using SetAlpha, SetBeta or
+ * SetMeanInvConc).
+ */
+
+class IIDBeta : public SimpleArray<double> {
+  public:
+    //! constructor parameterized by array size, and alpha and beta parameters
+    IIDBeta(int insize, const double& inmean, const double& ininvconc)
+        : SimpleArray<double>(insize), mean(inmean), invconc(ininvconc) {
+        Sample();
+    }
+
+    ~IIDBeta() {}
+
+    //! return value of the alpha parameter
+    double GetAlpha() const { return mean / invconc; }
+    //! return value of the beta parameter
+    double GetBeta() const { return (1.0-mean) / invconc; }
+
+    //! sample from prior
+    void Sample() {
+        for (int i = 0; i < GetSize(); i++) {
+            (*this)[i] = Random::BetaSample(GetAlpha(), GetBeta());
+        }
+    }
+
+    //! return log prior density of current values across array
+    double GetLogProb() const {
+        double total = 0;
+        for (int i = 0; i < GetSize(); i++) {
+            total += GetLogProb(i);
+        }
+        return total;
+    }
+
+    //! return log prior for entry i
+    double GetLogProb(int i) const { return Random::logBetaDensity(GetVal(i), GetAlpha(), GetBeta()); }
+
+    //! add current values stored in array to BetaSuffStat given as argument
+    void AddSuffStat(BetaSuffStat &suffstat) {
+        for (int i = 0; i < GetSize(); i++) {
+            suffstat.AddSuffStat(log(GetVal(i)), log(1 - GetVal(i)), 1);
+        }
+    }
+
+  protected:
+    const double& mean;
+    const double& invconc;
+};
+
+template <>
+struct has_custom_serialization<IIDBeta> : std::true_type {};
+
