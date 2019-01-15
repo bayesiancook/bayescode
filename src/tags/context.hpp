3d535a5d999766d006a42e2f63d686ef5689307d
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-01-15 14:00:58 +0100

    Added context union

--- src/tags/context.hpp
+++ src/tags/context.hpp
@@ -15,3 +15,26 @@ struct add_tag {
 
     using type = decltype(helper(_Context()));
 };
+
+template <class _Context, class _Context2>
+struct context_union {
+    static _Context helper(Context<>) {}  // _Context2 is empty
+
+    template <class Tag, class... Tags>
+    static typename context_union<typename add_tag<_Context, Tag>::type, Context<Tags...>>::type
+    helper(Context<Tag, Tags...>) {}
+
+    using type = decltype(helper(_Context2()));
+};
+
+// template <class _Context, class _Tag>
+// struct remove_tag {
+//     static _Context helper() {}  // empty context: nothing to remove
+
+//     template <class Arg, class... Args>
+//     static decltype(select(std::is_same<Arg, _Tag>(), arg, args)) helper(Arg arg, Args... args)
+//     {}
+
+//     template <class Arg, class... Args>
+//     static  select(std::true_type, Arg, Args...) {}
+// };
--- src/tags/test.cpp
+++ src/tags/test.cpp
@@ -1,9 +1,15 @@
 #include "doctest.h"
 
 #include "context.hpp"
+// #include <typeinfo>
+// #include <iostream>
+
+using namespace std;
 
 struct MyTag {};
 struct MyTag2 {};
+struct MyTag3 {};
+struct MyTag4 {};
 
 TEST_CASE("has_tag") {
     Context<MyTag2, MyTag> c;
@@ -28,4 +34,20 @@ TEST_CASE("add_tag") {
     add_tag<decltype(d), MyTag>::type e;
     bool e_has_mytag = has_tag<decltype(e), MyTag>::value;
     CHECK(e_has_mytag == true);
+}
+
+TEST_CASE("context_union") {
+    Context<MyTag> c;
+    Context<MyTag2, MyTag3> d;
+    context_union<decltype(c), decltype(d)>::type e;
+    // cout << typeid(e).name() << endl;
+
+    bool e_has_mytag = has_tag<decltype(e), MyTag>::value;
+    bool e_has_mytag2 = has_tag<decltype(e), MyTag2>::value;
+    bool e_has_mytag3 = has_tag<decltype(e), MyTag3>::value;
+    bool e_has_mytag4 = has_tag<decltype(e), MyTag4>::value;
+    CHECK(e_has_mytag == true);
+    CHECK(e_has_mytag2 == true);
+    CHECK(e_has_mytag3 == true);
+    CHECK(e_has_mytag4 == false);
 }
