7bc659be3dde9c16fecfe980dffd06d719a5a527
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-01-15 13:38:28 +0100

    Added add_tag

--- src/tags/context.hpp
+++ src/tags/context.hpp
@@ -2,11 +2,16 @@
 
 #include <type_traits>
 
-template <class Target, class... Tags>
-struct Context : public Tags... {
-    Target& target;
-    Context(Target& target) : target(target) {}
-};
+template <class... Tags>
+struct Context : public Tags... {};
 
 template <class _Context, class _Tag>
 struct has_tag : public std::integral_constant<bool, std::is_base_of<_Tag, _Context>::value> {};
+
+template <class _Context, class _Tag>
+struct add_tag {
+    template <class... Tags>
+    static Context<_Tag, Tags...> helper(Context<Tags...>) {}
+
+    using type = decltype(helper(_Context()));
+};
--- src/tags/test.cpp
+++ src/tags/test.cpp
@@ -2,15 +2,30 @@
 
 #include "context.hpp"
 
+struct MyTag {};
+struct MyTag2 {};
+
 TEST_CASE("has_tag") {
-    struct MyTag {};
-    struct MyTag2 {};
-    double a = 2;
-    Context<double, MyTag2, MyTag> c{a};
-    Context<double, MyTag2> d{a};
+    Context<MyTag2, MyTag> c;
+    Context<MyTag2> d;
 
     bool c_has_mytag = has_tag<decltype(c), MyTag>::value;
     CHECK(c_has_mytag == true);
     bool d_has_mytag = has_tag<decltype(d), MyTag>::value;
     CHECK(d_has_mytag == false);
+}
+
+TEST_CASE("add_tag") {
+    Context<> c;
+
+    bool c_has_mytag = has_tag<decltype(c), MyTag>::value;
+    CHECK(c_has_mytag == false);
+
+    add_tag<decltype(c), MyTag2>::type d;
+    bool d_has_mytag = has_tag<decltype(c), MyTag>::value;
+    CHECK(d_has_mytag == false);
+
+    add_tag<decltype(d), MyTag>::type e;
+    bool e_has_mytag = has_tag<decltype(e), MyTag>::value;
+    CHECK(e_has_mytag == true);
 }
