0f8bc331937e2e8eb01d862f6543931cd6976db2
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-03-01 14:02:41 +0100

    Cleaning in context.hpp

--- src/tags/context.hpp
+++ src/tags/context.hpp
@@ -3,63 +3,44 @@
 #include <tuple>
 #include <type_traits>
 
+/*==================================================================================================
+  Context
+  A context is a set of type tags.
+  It is meant to be used to encode metadata for individual declarations in introspection interfaces.
+==================================================================================================*/
+
+/*==================================================================================================
+  Context trait.
+  A type trait used to check that a type passed is a Context. */
 namespace is_context {
     struct tag {};
     template <class T>
     struct trait : public std::integral_constant<bool, std::is_base_of<tag, T>::value> {};
 }  // namespace is_context
 
+/*==================================================================================================
+  Context class. */
 template <class... Tags>
-struct Context : is_context::tag, public Tags... {
+class Context : is_context::tag, public Tags... {
     using Self = Context<Tags...>;
 
+    template <class... OtherTags>
+    static auto merge_helper(Context<OtherTags...>) {
+        return Context<Tags..., OtherTags...>();
+    }
+
+  public:
     template <class Tag>
     struct has_tag : public std::integral_constant<bool, std::is_base_of<Tag, Self>::value> {};
 
     template <class Tag>
-    static Context<Tag, Tags...> add_tag() {
+    static auto add_tag() {
         return Context<Tag, Tags...>();
     }
 
-    template <class... OtherTags>
-    static Context<Tags..., OtherTags...> merge_helper(Context<OtherTags...>) {
-        return Context<Tags..., OtherTags...>();
-    }
-
     template <class OtherContext>
-    static decltype(merge_helper(OtherContext())) merge(OtherContext) {
+    static auto merge(OtherContext) {
         static_assert(is_context::trait<OtherContext>::value, "OtherContext is not a context");
         return merge_helper(OtherContext());
     }
-
-
-    // template <class Tag>
-    // static decltype(
-    //     std::tuple_cat<std::conditional<std::is_same<Tag, Tags>::value, std::tuple<Tags>(), std::tuple<>()>>()...)
-    // remove_tag() {
-    //     return std::tuple_cat<
-    //         std::conditional<std::is_same<Tag, Tags>::value, std::tuple<Tags>(), std::tuple<>()>>()...;
-    // }
 };
-
-
-// template <class _Context, class _Tag>
-// struct remove_tag {
-//     static_assert(is_context::trait<_Context>::value, "_Context is not a context");
-
-//     template <class Arg, class... Args>
-//     static typename remove_tag<Context<Args...>, _Tag>::type  //
-//     select(std::true_type, Context<Arg, Args...>) {}
-
-//     template <class Arg, class... Args>
-//     static typename add_tag<typename remove_tag<Context<Args...>, _Tag>::type, Arg>::type  //
-//     select(std::false_type, Context<Arg, Args...>) {}
-
-//     static Context<> helper(Context<>) {}  // empty context: nothing to remove
-
-//     template <class Arg, class... Args>
-//     static decltype(select(typename std::is_same<Arg, _Tag>::type(), Context<Arg, Args...>())) //
-//     helper(Context<Arg, Args...>) {}
-
-//     using type = decltype(helper(_Context()));
-// };
