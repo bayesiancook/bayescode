503696a9ce5848e4e932879969111a4c8916bf06
bayesiancook <nicolas.lartillot@gmail.com>
2019-03-07 10:32:58 +0100

    misspelling corrected in -shrinkdposom option in readcodonm2a

--- sources/ReadCodonM2a.cpp
+++ sources/ReadCodonM2a.cpp
@@ -156,7 +156,7 @@ int main(int argc, char *argv[]) {
             } else if (s == "-shrinkposw")  {
                 i++;
                 shrinkposw = atof(argv[i]);
-            } else if (s == "-shrinkpdposom")  {
+            } else if (s == "-shrinkdposom")  {
                 i++;
                 shrinkdposom = atof(argv[i]);
             } else {
