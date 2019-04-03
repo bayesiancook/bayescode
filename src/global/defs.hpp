325486fef67ab5060b8662eafc5eb109e4e61fca
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-04-03 18:28:01 +0200

    Added a "global/defs.hpp" file with very common includes and aliases (eg "using std::vector")

---
+++ src/global/defs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <assert.h>
+#include <memory>
+#include <vector>
+#include "logging.hpp"
+using std::istream;
+using std::ostream;
+using std::string;
+using std::unique_ptr;
+using std::vector;
