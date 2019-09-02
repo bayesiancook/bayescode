eea05619368545c80d747b041378e19c49153eab
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-09-02 17:46:50 +0200

    More work on new globom

--- src/SingleOmega.cpp
+++ src/SingleOmega.cpp
@@ -19,12 +19,17 @@ int main(int argc, char* argv[]) {
     InferenceAppArgParse args(cmd);
     cmd.parse();
 
+    // parsing tree
+    std::ifstream tree_stream{args.treefile.getValue()};
+    NHXParser parser{tree_stream};
+    auto tree = make_from_parser(parser);
+
+    // random generator
     auto gen = make_generator();
 
     // model
-    auto globom = make_fixed_globom(0.1, 1.0, gen);
-    auto bl_array = make_branchlength_array(10, 1.0, gen);
-    
+    auto global_omega = make_fixed_globom(1.0, 1.0, gen);
+    auto branch_lengths = make_branchlength_array(parser, 0.1, 1.0);
 
     // initializing components
     ChainDriver chain_driver{cmd.chain_name(), args.every.getValue(), args.until.getValue()};
--- src/global_omega.hpp
+++ src/global_omega.hpp
@@ -8,6 +8,7 @@
 #include "bayes_toolbox/src/structure/model.hpp"
 #include "bayes_toolbox/src/structure/node.hpp"
 #include "bayes_toolbox/utils/tagged_tuple/src/fancy_syntax.hpp"
+#include "tree/implem.hpp"
 
 TOKEN(omega)
 
@@ -38,9 +39,18 @@ void move_globom(GlobomModel& model, Lambda& children_logprob, Gen& gen) {
 
 TOKEN(bl_array)
 
-template <class Gen>
-auto make_branchlength_array(size_t nb_branches, double hypermean, Gen& gen) {
-    auto bl_array = make_node_array<exponential>(nb_branches, n_to_constant(hypermean));
-    draw(bl_array, gen);
+/* Array of branch lengths, gamma iid with fixed mean and invshape. */
+auto make_branchlength_array(TreeParser& parser, double mean, double invshape) {
+    DEBUG("Getting branch lengths from tree");
+    const size_t nb_branches = parser.get_tree().nb_nodes();
+    auto initial_bl = branch_container_from_parser<double>(
+        parser, [](int i, const auto& tree) { return stod(tree.tag(i, "length")); });
+
+    DEBUG("Creating branch length array of gamma nodes (length {})", nb_branches);
+    auto bl_array = make_node_array<gamma_ss>(
+        nb_branches, n_to_constant(1. / invshape), n_to_constant(mean * invshape));
+    set_value(bl_array, initial_bl);
+
+    // return model
     return make_model(bl_array_ = std::move(bl_array));
 }
