7cc17346e8dbfb094b9f4da2c238e64ec1a39489
bayesiancook <nicolas.lartillot@gmail.com>
2021-04-14 20:12:30 +0200

    simplified output of readmultigeneglobom -dsomss

--- sources/ReadMultiGeneSingleOmega.cpp
+++ sources/ReadMultiGeneSingleOmega.cpp
@@ -119,7 +119,6 @@ class MultiGeneSingleOmegaSample : public MultiGeneSample {
 
         GetModel()->MasterReceiveGeneArray(array);
         ofstream os((name + ".genebranchdsomsuffstat").c_str());
-        // os << GetModel()->GetNgene() << '\n';
         for (int i=0; i<GetModel()->GetNgene(); i++) {
             os << array[i] << '\n';
         }
@@ -130,39 +129,7 @@ class MultiGeneSingleOmegaSample : public MultiGeneSample {
             globdsomss.Add(array[i]);
         }
 
-        /*
-        SimpleBranchArray<double> totS(GetModel()->GetTree(), 0);
-        SimpleBranchArray<double> totN(GetModel()->GetTree(), 0);
-        for (int i=0; i<GetModel()->GetNgene(); i++) {
-            int nsite = GetModel()->GetLocalGeneNsite(i);
-            SimpleBranchArray<double> tmpS(GetModel()->GetTree(), 0);
-            SimpleBranchArray<double> tmpN(GetModel()->GetTree(), 0);
-            array[i].GetdS(tmpS);
-            array[i].GetdN(tmpN);
-            for (int i=0; i<GetModel()->GetTree().GetNbranch(); i++)  {
-                totS[i] += nsite * tmpS[i];
-                totN[i] += nsite * tmpN[i];
-            }
-        }
-        SimpleBranchArray<double> meanofratios(GetModel()->GetTree(), 0);
-        for (int i=0; i<GetModel()->GetTree().GetNbranch(); i++)  {
-            if (totS[i])    {
-                meanofratios[i] = totN[i] / totS[i];
-            }
-            else    {
-                meanofratios[i] = 0;
-            }
-        }
-        SimpleBranchArray<double> ratioofmeans(GetModel()->GetTree(), 0);
-        globdsomss.GetdNdS(ratioofmeans);
-        ofstream compos((name + ".compdNdS").c_str());
-        for (int i=0; i<GetModel()->GetTree().GetNbranch(); i++)  {
-            compos << ratioofmeans[i] << '\t' << meanofratios[i] << '\n';
-        }
-        */
-
         ofstream gos((name + ".meanbranchdsomsuffstat").c_str());
-        // gos << "1\n";
         gos << globdsomss << '\n';
         cerr << "global dsom path suffstats in " << name << ".meanbranchdsomsuffstat\n";
 
@@ -174,7 +141,6 @@ class MultiGeneSingleOmegaSample : public MultiGeneSample {
 
         GetModel()->MasterReceiveGeneArray(gcconsarray);
         ofstream gcos((name + ".genebranchgcconsdsomsuffstat").c_str());
-        // gcos << GetModel()->GetNgene() << '\n';
         for (int i=0; i<GetModel()->GetNgene(); i++) {
             gcos << gcconsarray[i] << '\n';
         }
@@ -184,7 +150,6 @@ class MultiGeneSingleOmegaSample : public MultiGeneSample {
             gcconsglobdsomss.Add(gcconsarray[i]);
         }
         ofstream gcgos((name + ".meanbranchgcconsdsomsuffstat").c_str());
-        // gos << "1\n";
         gcgos << gcconsglobdsomss << '\n';
         cerr << "global GC-cons dsom path suffstats in " << name << ".meanbranchgcconsdsomsuffstat\n";
 
