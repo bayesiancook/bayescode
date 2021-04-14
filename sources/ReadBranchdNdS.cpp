6eb102c655f2e3dfb4001b45215f02723bfd55a4
bayesiancook <nicolas.lartillot@gmail.com>
2021-04-14 20:13:51 +0200

    readbranchdnds: branch vals = dS and nodevals = dNdS

--- sources/ReadBranchdNdS.cpp
+++ sources/ReadBranchdNdS.cpp
@@ -5,50 +5,61 @@
 #include <iostream>
 #include <string>
 
+void RecursiveToNewick(ostream& os, const Link* from, const BranchSelector<double>& ds, const BranchSelector<double>& dnds) {
+    if (from->isLeaf()) {
+        os << from->GetNode()->GetName();
+        os << "_";
+    }
+    else    {
+        os << "(";
+        for (const Link* link=from->Next(); link!=from; link=link->Next())  {
+            RecursiveToNewick(os, link->Out(), ds, dnds);
+            if (link->Next() != from)   {
+                os << ",";
+            }
+        }
+        os << ")";
+    }
+    if (! from->isRoot())	{
+        os << dnds.GetVal(from->GetBranch()->GetIndex());
+        os << ":";
+        os << ds.GetVal(from->GetBranch()->GetIndex());
+    }
+    else    {
+        os << dnds.GetVal(from->Next()->GetBranch()->GetIndex());
+    }
+}
+
+void ToNewick(ostream& os, const BranchSelector<double>& ds, const BranchSelector<double>& dnds)    {
+    RecursiveToNewick(os, ds.GetTree().GetRoot(), ds, dnds);
+    os << ";\n";
+}
+
 int main(int argc, char* argv[])    {
 
-    string infile = argv[1];
-    int burnin = atoi(argv[2]);
-    int until = atoi(argv[3]);
-    string treefile = argv[4];
-    string outfile = argv[5];
+    string suffstatfile = argv[1];
+    string treefile = argv[2];
+    string outfile = argv[3];
 
     Tree tree(treefile);
     tree.SetIndices();
 
-
-    ifstream is(infile.c_str());
-    cerr << "burnin\n";
-    for (int i=0; i<burnin; i++)    {
-        cerr << '.';
-        string s;
-        getline(is,s);
-    }
-    cerr << '\n';
-
-    cerr << "reading\n";
     dSOmegaPathSuffStatBranchArray dsomss(tree);
-    for (int i=0; i<until-burnin; i++)  {
-        cerr << '.';
-        dSOmegaPathSuffStatBranchArray tmp(tree);
-        is >> tmp;
-        dsomss.Add(tmp);
-    }
-    cerr << '\n';
-
-    DistBranchArray<double> val(tree);
-    SimpleBranchArray<double> tmp(tree);
-    dsomss.GetdNdS(tmp);
-    val.Add(tmp);
-    // val.Sort();
-
-    ofstream os((outfile + ".tab").c_str());
-    val.TabulateMean(os, 0, 1);
+    ifstream is(suffstatfile.c_str());
+    is >> dsomss;
+    SimpleBranchArray<double> dnds(tree);
+    dsomss.GetdNdS(dnds);
+    SimpleBranchArray<double> ds(tree);
+    dsomss.GetdS(ds);
 
     ofstream tos((outfile + ".tre").c_str());
-    val.NodeMeanToStream(tos);
-
-    cerr << "tabulated leaf values in " << outfile << ".tab\n";
+    ToNewick(tos, ds, dnds);
     cerr << "newick tree in " << outfile << ".tre\n";
+
+    /*
+    ofstream os((outfile + ".tab").c_str());
+    val.TabulateMean(os, 0, 1);
+    cerr << "tabulated leaf values in " << outfile << ".tab\n";
+    */
 }
 
